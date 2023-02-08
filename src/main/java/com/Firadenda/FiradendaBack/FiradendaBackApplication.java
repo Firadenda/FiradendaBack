07f9b58b1cb9018a7d7cfb4f3bab6866288458d1
BlueSky <nathantomasian@gmail.com>
2023-02-08 13:30:24 +0100

    Starter commit

---
+++ src/main/java/com/Firadenda/FiradendaBack/FiradendaBackApplication.java
@@ -0,0 +1,13 @@
+package com.Firadenda.FiradendaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiradendaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiradendaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Firadenda/FiradendaBack/FiradendaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Firadenda.FiradendaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiradendaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
