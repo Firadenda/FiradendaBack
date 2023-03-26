c5bcfa30d6a47eda0935c3e228133f82f7e356ff
RKS-BlueSky <60184677+RKS-BlueSky@users.noreply.github.com>
2023-03-27 00:55:09 +0200

    Access

--- src/main/java/com/Firadenda/FiradendaBack/controller/CategoryController.java
+++ src/main/java/com/Firadenda/FiradendaBack/controller/CategoryController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path="/category")
 public class CategoryController {
 
